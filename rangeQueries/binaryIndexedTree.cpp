4e32ec24743fefde48b2697ddc2f5bc6a9a273b2
Phan Ngưng <72738828+77x17@users.noreply.github.com>
2023-10-07 08:56:36 +0700

    Update binaryIndexedTree.cpp

--- rangeQueries/binaryIndexedTree.cpp
+++ rangeQueries/binaryIndexedTree.cpp
@@ -1,60 +1,44 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
 
-const int MAX_N = 1e6 + 1;
+const int MAX_N = 2e5 + 1;
 
 struct FenwickTree {
-    int n, val[MAX_N];
+    int n;
+    long long val[MAX_N];
 
     void Init(int _n) {
         n = _n;
-
-        fill(val + 1, val + _n + 1, 0);
     }
 
-    void Update(int x, int v) {
-        for (; x <= n; x += x & -x) val[x] += v;
+    void Update(int x, int c) {
+        for (; x <= n; x += x & -x) val[x] += c;
     }
 
-    int Get(int x) {
-        int result = 0;
+    long long Get(int x) {
+        long long result = 0;
         for (; x >= 1; x -= x & -x) result += val[x];
         return result;
     }
-} bit;
+} BIT;
 
-int n;
-
-void Input() {
-    cin >> n;
-}
-
-void Process() {
-    bit.Init(n);
-
-    int q; cin >> q; while (q--) {
-        int k; cin >> k;
-
-        if (k == 0) {
-            int x, v; cin >> x >> v;
-
-            bit.Update(x, v);
-        }
-
-        if (k == 1) {
-            int x; cin >> x;
-
-            cout << bit.Get(x) << '\n';
-        }
-    }
-}
+int n, m;
+int arr[MAX_N];
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+    if (fopen("Input.txt", "r")) freopen("Input.txt", "r", stdin);
 
-    Input();
+    std::cin >> n >> m;
+    for (int i = 1; i <= n; i++) std::cin >> arr[i];
 
-    Process();
+    BIT.Init(n);
+    for (int i = 1; i <= n; i++) BIT.Update(i, arr[i]);
+
+    while (m--) {
+        int l, r; std::cin >> l >> r;
+
+        std::cout << BIT.Get(r) - BIT.Get(l - 1) << '\n';
+    }
 
     return 0;
 }
