9e56751cc022f41f92c3b09d2979fc3af98c3f24
Phan Ngưng <72738828+77x17@users.noreply.github.com>
2023-10-11 22:45:52 +0700

    Update lowestCommonAncestor.cpp

--- graphAlgorithms/lowestCommonAncestor.cpp
+++ graphAlgorithms/lowestCommonAncestor.cpp
@@ -1,100 +1,58 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
-#define fi first
+const int MAX_N = 2e5;
+const int LOG = 18;
 
-#define se second
+int n, m;
+int par[MAX_N + 1][LOG + 1];
+std::vector<int> adj[MAX_N + 1];
 
-using namespace std;
-
-const int MAX_N = 300000;
-
-
-int n, LOG;
-
-vector<pair<int, int>> adj[MAX_N + 1];
-
-void Input() {
-    cin >> n; LOG = log(n) / log(2);
-
-    for (int i = 1; i < n; i++) {
-        int u, v, c; cin >> u >> v >> c;
-
-        adj[u].push_back({c, v}); adj[v].push_back({c, u});
-    }
-}
-
-
-vector<int> high; vector<long long> dist; vector<vector<int>> par;
+int high[MAX_N + 1];
 
 void Dfs(int u) {
-    for (pair<int, int> v : adj[u]) if (v.se != par[u][0]) {
-        high[v.se] = high[u] + 1; dist[v.se] = dist[u] + v.fi; par[v.se][0] = u;
-
-        Dfs(v.se);
-    }
+	for (int v : adj[u]) {
+		if (v != par[u][0]) {
+			high[v] = high[u] + 1;
+			Dfs(v);	
+		}
+	}
 }
 
-int findLCA(int u, int v) {
-    if (high[u] < high[v]) return LCA(v, u);
-
-    for (int j = LOG; j >= 0; j--) if (high[par[u][j]] >= high[v]) u = par[u][j];
-
-    if (u == v) return u;
-
-    for (int j = LOG; j >= 0; j--) if (par[u][j] != par[v][j]) u = par[u][j], v = par[v][j];
-
-    return par[u][0];
+int find_LCA(int u, int v) {
+	if (high[u] < high[v]) return find_LCA(v, u);
+	for (int j = LOG; j >= 0; j--) {
+		if (high[par[u][j]] >= high[v]) u = par[u][j];
+	}
+	if (u == v) return u;
+	for (int j = LOG; j >= 0; j--) {
+		if (par[u][j] != par[v][j]) u = par[u][j], v = par[v][j];
+	}
+	return par[u][0];
 }
-
-void prepareLCA() {
-    high.assign(n + 1, 0); high[0] = -1; dist.assign(n + 1, 0); par.assign(n + 1, vector<int> (LOG + 1, 0));
-
-    Dfs(1);
-
-    for (int j = 1; j <= LOG; j++) for (int i = 1; i <= n; i++) par[i][j] = par[par[i][j - 1]][j - 1];
-}
-
-void Process() {
-    prepareLCA();
-
-    int q; cin >> q;
-
-    while (q--) {
-        int u, v; cin >> u >> v;
-
-        cout << dist[u] + dist[v] - 2 * dist[findLCA(u, v)] << '\n';
-    }
-}
-
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	std::ios_base::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+	if (fopen("Input.txt", "r")) freopen("Input.txt", "r", stdin);
 
-    Input();
+	std::cin >> n >> m;
+	for (int i = 2; i <= n; i++) {
+		std::cin >> par[i][0];
+		adj[par[i][0]].push_back(i);
+	}
 
-    Process();
+	high[0] = -1; 
+	Dfs(1);
+	for (int j = 1; j <= LOG; j++) {
+		for (int i = 1; i <= n; i++) {
+			par[i][j] = par[par[i][j - 1]][j - 1];
+		}
+	} 
 
-    return 0;
+	while (m--) {
+		int u, v;
+		std::cin >> u >> v;
+
+		std::cout << find_LCA(u, v) << '\n';
+	}
 }
-
-/*
-
-Input:
-8
-1 2 2
-2 6 2
-1 3 7
-3 4 1
-3 5 9
-4 7 9
-1 8 7
-3
-5 7
-6 1
-3 8
-Output:
-19
-4
-14
-
-*/
